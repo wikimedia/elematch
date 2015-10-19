f6f0feadc02054cdd2505e2c5654be9e38d1a573
Gabriel Wicke <gwicke@wikimedia.org>
2015-10-19 16:03:01 -0700

    Add innerHTML support

--- test/index.js
+++ test/index.js
@@ -10,16 +10,22 @@ var matcher = new ElementMatcher({
     'foo-bar': id,
 });
 
+var testElement = "<test-element foo='bar' baz=\"booz\"><foo-bar></foo-bar><span>hello</span></test-element>";
+
+function innerHTML(s) {
+    return s.replace(/^<[^>]+>(.*)<\/[^>]+>$/, '$1');
+}
+
 var testDoc = "<doctype html><head><title>hello</title></head>\n"
-        + "<body><test-element foo='bar' baz=\"booz\"><foo-bar></foo-bar><span>hello</span></test-element></body>";
+        + testElement + "<body></body>";
 
 module.exports = {
     "basic matching": {
         "custom element": function() {
             var nodes = matcher.matchAll(testDoc);
             var n0 = nodes[0];
-            //assert.equal(n0.innerHTML, '<span>hello</span>');
-            assert.equal(n0.outerHTML, "<test-element foo='bar' baz=\"booz\"><foo-bar></foo-bar><span>hello</span></test-element>");
+            assert.equal(n0.innerHTML, innerHTML(testElement));
+            assert.equal(n0.outerHTML, testElement);
             assert.deepEqual(n0.attributes, {
                 foo: 'bar',
                 baz: 'booz'
