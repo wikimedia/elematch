2464ba68e4943eb7088cf2f3a5034d48cb96a25c
Gabriel Wicke <gwicke@wikimedia.org>
2016-09-23 13:41:25 -0700

    Set up browserify build for client-side use
    - Drop the external entity package, reducing client-side bundle size
    from 70+kb to ~15kb.
    - Only pull in ReadableStream shim in node.
    - Point browserify uses to the minimal browser build in package.json.
    
    The resulting bundle is 15k uncompressed & un-minified, and about 4k
    gzip compressed.

--- build/matcher.js
+++ build/matcher.js
@@ -1,9 +1,9 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-// Shim ReadableStream in node
-if (!global.ReadableStream) {
-    var ReadableStream = require('node-web-streams').ReadableStream;
-}
+// XXX: Only include this when using node. Skip when compiling with
+// browserify.
+const ReadableStream = require('node-web-streams').ReadableStream;
 
 // Shared patterns
 const optionalAttributePattern = '(?:\\s+[a-zA-Z_-]+(?:=(?:"[^"]*"|\'[^\']*\'))?)*';
@@ -391,3 +391,5 @@ class Matcher {
 }
 
 module.exports = Matcher;
+
+},{"node-web-streams":undefined}]},{},[1]);
